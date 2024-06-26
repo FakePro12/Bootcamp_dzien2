bb1a5679981865d1f6f6c137d12050df8457caa7
FakePro12echo <igorskulski123@gmail.com>
2024-06-26 19:10:34 +0200

    upiększenie blogu

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">odśwież</button>
-        SIEMA W MOIM BLOGU !
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-green-700">Wpisy na bloga:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-green-700 rounded text-white p-4">odśwież</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+        <div v-for="wpis in wpisy" class="grid drop-shadow-xl bg-stone-300 p-4">
+            <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+        <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+        <button class="bg-green-700 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+    </div>
     </div>
 </template>
 
@@ -26,6 +34,8 @@ export default {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
-
+    async mounted(){
+        this.pobierzWpisy()
+    }
 }
 </script>
